22af543569b563407c4447ef0ebb460c66e3ffa5
Hyeonhak_Do <ehgusgkr6@naver.com>
2024-11-14 22:38:57 +0900

    [Create] Create xcode file

---
+++ Shortkathon_Project/Shortkathon_ProjectTests/Shortkathon_ProjectTests.swift
@@ -0,0 +1,17 @@
+//
+//  Shortkathon_ProjectTests.swift
+//  Shortkathon_ProjectTests
+//
+//  Created by 도현학 on 11/14/24.
+//
+
+import Testing
+@testable import Shortkathon_Project
+
+struct Shortkathon_ProjectTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ Shortkathon_Project/Shortkathon_ProjectUITests/Shortkathon_ProjectUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  Shortkathon_ProjectUITestsLaunchTests.swift
+//  Shortkathon_ProjectUITests
+//
+//  Created by 도현학 on 11/14/24.
+//
+
+import XCTest
+
+final class Shortkathon_ProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
