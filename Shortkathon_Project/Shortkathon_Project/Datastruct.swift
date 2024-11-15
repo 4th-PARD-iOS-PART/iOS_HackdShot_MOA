12e5ba58302460122f838be28d4b1b54dd9e2dc7
Hyeonhak_Do <ehgusgkr6@naver.com>
2024-11-16 04:14:09 +0900

    edit ViewController, SecondViewController

--- Shortkathon_Project/Shortkathon_Project/Datastruct.swift
+++ Shortkathon_Project/Shortkathon_Project/Datastruct.swift
@@ -17,8 +17,13 @@ struct Task {
     let taskName: String
 }
 
+struct Project {
+    let projectName: String
+}
+
 
 struct DataStruct {
     static var names: [Name] = []
     static var tasks: [Task] = []
+    static var projects: [Project] = []
 }
